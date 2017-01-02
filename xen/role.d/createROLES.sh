6182efff6320243e84e0811323513f9bb719f0de
value should not be mesured in $ but in resuced lifes <mail@marclandolt.ch>
2017-01-02 19:42:29 +0100

    sorted

--- xen/role.d/createROLES.sh
+++ xen/role.d/createROLES.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-cp default /etc/xen-tools/role.d/
 cp pdns /etc/xen-tools/role.d/
+cp default /etc/xen-tools/role.d/
 cp zabbix /etc/xen-tools/role.d/
+
+chmod u+x /etc/xen-tools/role.d/pdns
+chmod u+x /etc/xen-tools/role.d/default
+chmod u+x /etc/xen-tools/role.d/zabbix
