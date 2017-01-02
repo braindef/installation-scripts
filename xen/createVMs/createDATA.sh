caf60a179d1ae1ac05bf50f62319eb1ad48fd761
root <root@debian>
2017-01-02 23:53:21 +0100

    fixing mistakes

--- xen/createVMs/createDATA.sh
+++ xen/createVMs/createDATA.sh
@@ -5,4 +5,4 @@
 #/usr/bin/xen-create-image --hostname data --ip 192.168.179.203 --gateway 192.168.179.1 --netmask 255.255.255.0 --size=5Gb --swap=1Gb --memory=1024Mb --arch=amd64 --role=udev,default --dir=/home/xen --genpass_len=8 --verbose
 
 
-/usr/bin/xen-create-image --hostname data --ip 192.168.179.203 --gateway 192.168.179.1 --netmask 255.255.255.0 --size=5Gb --noswap --swap=1Gb --image-dev=/dev/sdb --memory=1024Mb --arch=amd64 --role=udev,default --dir=/home/xen --password=1234567 --verbose
+/usr/bin/xen-create-image --hostname data --ip 192.168.179.203 --gateway 192.168.179.1 --netmask 255.255.255.0 --size=5Gb  --swap=1Gb --memory=1024Mb --arch=amd64 --role=udev,default --dir=/home/xen --password=1234567 --verbose
--- xen/createVMs/createZABBIX.sh
+++ xen/createVMs/createZABBIX.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-/usr/bin/xen-create-image --hostname zabbix --ip 192.168.179.204 --gateway 192.168.179.1 --netmask 255.255.255.0 --size=20Gb --swap=4Gb --memory=2048Mb --arch=amd64 --role=udev,default,zabbix --dir=/home/xen --genpass_len=8 --verbose
+#/usr/bin/xen-create-image --hostname zabbix --ip 192.168.179.204 --gateway 192.168.179.1 --netmask 255.255.255.0 --size=20Gb --swap=4Gb --memory=2048Mb --arch=amd64 --role=udev,default,zabbix --dir=/home/xen --genpass_len=8 --verbose
+/usr/bin/xen-create-image --hostname zabbix --ip 192.168.179.204 --gateway 192.168.179.1 --netmask 255.255.255.0 --size=20Gb --swap=4Gb --memory=2048Mb --arch=amd64 --role=udev,default,zabbix --dir=/home/xen --passwd=1234567 --verbose
