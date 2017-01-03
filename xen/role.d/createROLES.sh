668e06511c18ec86bc121071fba3c2e540af0464
root <root@debian>
2017-01-03 01:48:59 +0100

    several changes

--- xen/role.d/createROLES.sh
+++ xen/role.d/createROLES.sh
@@ -3,7 +3,11 @@
 cp pdns /etc/xen-tools/role.d/
 cp default /etc/xen-tools/role.d/
 cp zabbix /etc/xen-tools/role.d/
+cp monitoring-client /etc/xen-tools/role.d/
+cp development /etc/xen-tools/role.d/
 
 chmod u+x /etc/xen-tools/role.d/pdns
 chmod u+x /etc/xen-tools/role.d/default
 chmod u+x /etc/xen-tools/role.d/zabbix
+chmod u+x /etc/xen-tools/role.d/monitoring-client
+chmod u+x /etc/xen-tools/role.d/development
