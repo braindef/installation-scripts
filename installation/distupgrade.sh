15f47befb5452a751627d046d599f69389ad0b12
root <root@debian>
2017-01-03 05:59:35 +0100

    upgrade script

--- installation/distupgrade.sh
+++ installation/distupgrade.sh
@@ -7,4 +7,8 @@ sed -e 's/jessie/stretch/g' /etc/apt/sources.list
 sed -i -e 's/jessie/stretch/g' /etc/apt/sources.list
 
 
+apt-get update
+apt-get dist-upgrade
+apt-get upgrade
+apt-get autoremove
 
