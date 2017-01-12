c166df065ee52020e8bd17424e66d71e7453525f
root <root@debian>
2017-01-12 04:10:11 +0100

    pamblue

--- installation/pamblue.sh
+++ installation/pamblue.sh
@@ -3,4 +3,7 @@
 hcitool scan |grep -v Scann >>/etc/security/bluesscan.conf
 vim /etc/security/bluesscan.conf
 
+echo '#auth    sufficient                      pam_blue.so' >>vim /etc/pam.d/common-auth
+echo '#auth    requisite                      pam_blue.so' >>vim /etc/pam.d/common-auth
+
 vim /etc/pam.d/common-auth
