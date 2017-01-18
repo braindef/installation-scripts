9fd26e69bbb74d573284f024afc4a1c80dcda7c3
Marc Landolt <mail@marclandolt.ch>
2017-01-18 06:18:03 +0100

    Create ooni-stretch.sh

---
+++ tor/ooni-stretch.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+sudo apt-get install python-pip python-virtualenv virtualenv
+sudo apt-get install libgeoip-dev libffi-dev libdumbnet-dev libssl-dev libpcap-dev
+git clone https://github.com/TheTorProject/ooni-probe
+cd ooni-probe
+virtualenv venv
+
+source venv/bin/activate
+pip install -r requirements.txt
+pip install -r requirements-dev.txt
+python setup.py install
+ooniprobe -s
