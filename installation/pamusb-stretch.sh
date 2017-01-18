38f5bcccb02df42b0730a3deeeeed4a922ef7349
root <root@debian>
2017-01-18 05:09:20 +0100

    networkScripts

---
+++ installation/pamusb-stretch.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+#somewhere on github.com
--- tor/rename-interfaces.sh
+++ tor/rename-interfaces.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo -n "# "
-ifconfig | grep -a2 enp  >> /etc/udev/rules.d/70-persistent-net.rules
+#echo -n "# " >>
+ifconfig | grep ether  >> /etc/udev/rules.d/70-persistent-net.rules
 vim /etc/udev/rules.d/70-persistent-net.rules
 
