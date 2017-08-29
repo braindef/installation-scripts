0cd2419d8c5b6587df3bbfe7443fde88fb578251
Marc Landolt jun <mail@marclandolt.ch>
2017-08-29 23:58:55 +0000

    zabbix monitoring script

---
+++ monitoring/debian/stretch/dns_check.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+dns_server=212.60.61.246
+
+dns_query=$(dig $1 | grep "^$1")
+if [[ $dns_query == *"$1"* ]]
+then
+	dig $1 @$dns_server | grep "Query"|cut -d: -f2 |cut -dm -f1 |bc -l
+else
+	exit 1
+fi
+
