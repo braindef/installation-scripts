dfd14e7874ce9c3772c814684270b53befbf9f49
Marc Landolt <mail@marclandolt.ch>
2018-06-08 16:01:12 +0200

    Create 8_mirror.sh

---
+++ desktop/gentoo/8_mirror.sh
@@ -0,0 +1,3 @@
+mirrorselect -i -o >> /mnt/gentoo/etc/portage/make.conf
+mkdir --parents /mnt/gentoo/etc/portage/repos.conf
+cp /mnt/gentoo/usr/share/portage/config/repos.conf /mnt/gentoo/etc/portage/repos.conf/gentoo.conf
