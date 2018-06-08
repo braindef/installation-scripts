e7b67e2178383c634ad4f0799ab747df76c5d81c
Marc Landolt <mail@marclandolt.ch>
2018-06-08 15:51:04 +0200

    Create getStage3.sh

---
+++ desktop/gentoo/getStage3.sh
@@ -0,0 +1,3 @@
+wget http://distfiles.gentoo.org/releases/amd64/autobuilds/20180607T214503Z/stage3-amd64-20180607T214503Z.tar.xz
+unxz http://distfiles.gentoo.org/releases/amd64/autobuilds/20180607T214503Z/stage3-amd64-20180607T214503Z.tar.xz
+tar xpf stage3-*.tar --xattrs-include='*.*' --numeric-owner
