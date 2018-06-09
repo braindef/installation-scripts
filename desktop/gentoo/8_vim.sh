acb4101cc7b23c0121c1b684827251bc4f5a76d4
Marc jun Landolt <mail@marclandolt.ch>
2018-06-09 00:15:39 +0000

    gentoo

--- desktop/gentoo/8_vim.sh
+++ desktop/gentoo/8_vim.sh
@@ -2,4 +2,8 @@ cp make.conf /etc/portage/
 eselect profile list
 eselect profile set 18
 emerge --ask --update --deep --newuse @world
-emerge --ask vim git sudo
+USE="systemd" emerge --ask --update --deep --newuse @world
+emerge --ask vim dev-vcs/git sudo
+
+USE=" gusb introspection policykit systemd" emerge --ask --update --deep --newuse @world
+
