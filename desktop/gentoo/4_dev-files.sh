c806c50820cff6cd23fc080d77e44822723c973c
Marc jun Landolt <mail@marclandolt.ch>
2018-06-10 12:28:12 +0000

    locale

--- desktop/gentoo/4_dev-files.sh
+++ desktop/gentoo/4_dev-files.sh
@@ -3,3 +3,9 @@ mount --rbind /sys /mnt/gentoo/sys
 mount --make-rslave /mnt/gentoo/sys
 mount --rbind /dev /mnt/gentoo/dev
 mount --make-rslave /mnt/gentoo/dev
+
+echo '
+LANG="en_US.UTF-8
+LC_COLLATE="C"
+' > /etc/env.d/02locale
+
