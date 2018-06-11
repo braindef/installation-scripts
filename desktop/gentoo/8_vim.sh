34b1d54e0d9ca855ea441a61b784fff6af72ea9e
Marc Landolt <mail@marclandolt.ch>
2018-06-11 09:10:59 +0200

    Update 8_vim.sh

--- desktop/gentoo/8_vim.sh
+++ desktop/gentoo/8_vim.sh
@@ -2,12 +2,16 @@
 echo "LANG=en_US.UTF-8" > /etc/default/locale
 
 cp make.conf /etc/portage/
-eselect profile list
-eselect profile set 18
-emerge --ask --update --deep --newuse @world
-USE="systemd" emerge --ask --update --deep --newuse @world
-USE="systemd" emerge --ask vim dev-vcs/git sudo
 
-USE="systemd gusb introspection policykit systemd" emerge --ask --update --deep --newuse @world
-USE="systemd" emerge gdm gnome gnome-terminal nautilus chromium --ask
+nano /etc/portage/make.conf
+
+eselect profile set 18
+
+emerge --update --deep --newuse @world
+
+emerge --update vim dev-vcs/git sudo
+
+emerge --update --deep --newuse @world
+
+emerge --update gdm gnome gnome-terminal nautilus chromium
 
