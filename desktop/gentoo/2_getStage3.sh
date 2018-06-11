7c6062d2037c81353e32947c8f455f1bb3454ae6
Marc jun Landolt <mail@marclandolt.ch>
2018-06-11 12:18:27 +0000

    changes

--- desktop/gentoo/2_getStage3.sh
+++ desktop/gentoo/2_getStage3.sh
@@ -1,3 +1,4 @@
+cd /mnt/gentoo
 wget http://distfiles.gentoo.org/releases/amd64/autobuilds/20180607T214503Z/stage3-amd64-20180607T214503Z.tar.xz
 unxz stage3-amd64-20180607T214503Z.tar.xz
 tar xpf stage3-*.tar --xattrs-include='*.*' --numeric-owner
