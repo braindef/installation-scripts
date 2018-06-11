1e42a9abb0b329b7e22810417921defe77827569
Marc Landolt <mail@marclandolt.ch>
2018-06-11 05:05:10 +0200

    Update 11_grub.sh

--- desktop/gentoo/11_grub.sh
+++ desktop/gentoo/11_grub.sh
@@ -3,6 +3,7 @@ USE="systemd" emerge --ask sys-kernel/gentoo-sources
 cd /usr/src/linux
 
 echo asus croshair VI Board needs the IGB (intel network driver) need to CONFIG_IGB=y and then menuconfig again
+echo edimax usb needs CONFIG_RTL8192CU=m and also a make menuconfig again
 
 make menuconfig
 
