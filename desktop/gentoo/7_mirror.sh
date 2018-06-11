4e60bf95dec4165531b759f14cbd838f76779e67
Marc jun Landolt <mail@marclandolt.ch>
2018-06-11 15:10:04 +0000

    changes

--- desktop/gentoo/7_mirror.sh
+++ desktop/gentoo/7_mirror.sh
@@ -1,4 +1,4 @@
-mirrorselect -i -o >> /mnt/gentoo/etc/portage/make.conf
+#mirrorselect -i -o >> /mnt/gentoo/etc/portage/make.conf
 mkdir --parents /mnt/gentoo/etc/portage/repos.conf
-cp /mnt/gentoo/usr/share/portage/config/repos.conf /mnt/gentoo/etc/portage/repos.conf/gentoo.conf
+cp -v /usr/share/portage/config/repos.conf /mnt/gentoo/etc/portage/repos.conf/gentoo.conf
 emerge-webrsync
