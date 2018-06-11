9b346369e0fc98cc1dfcd9cab520a8674ec7d55b
Marc jun Landolt <mail@marclandolt.ch>
2018-06-11 12:15:05 +0000

    changes

--- desktop/gentoo/2_getStage3.sh
+++ desktop/gentoo/2_getStage3.sh
@@ -2,4 +2,4 @@ wget http://distfiles.gentoo.org/releases/amd64/autobuilds/20180607T214503Z/stag
 unxz stage3-amd64-20180607T214503Z.tar.xz
 tar xpf stage3-*.tar --xattrs-include='*.*' --numeric-owner
 
-cp /installation-scripts/desktop/gentoo/make.conf /mnt/gentoo/etc/portage/make.conf
+cp ./installation-scripts/desktop/gentoo/make.conf /mnt/gentoo/etc/portage/make.conf
