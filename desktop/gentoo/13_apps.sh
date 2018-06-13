14e3efe3ebf7bf320e977cb2ce91aab2be855a48
Marc jun Landolt <mail@marclandolt.ch>
2018-06-13 14:20:34 +0200

    added gentoo install

--- desktop/gentoo/13_apps.sh
+++ desktop/gentoo/13_apps.sh
@@ -13,3 +13,22 @@ emerge --update app-portage/gentoolkit-next
 vim /etc/locale.conf
 systemctl enable dbus
 
+echo ' you should add
+LC_CTYPE="en_US.UTF-8"
+LC_ALL="en_US.UTF-8"
+LANG="en_US.UTF-8"
+to your /etc/default/locale'
+
+echo ' you should add
+Section "InputClass"
+        Identifier "touchpad catchall"
+        Driver "synaptics"
+        MatchIsTouchpad "on"
+        Option "VertEdgeScroll" "on"
+        Option      "CircularScrolling"         "on"
+        Option      "VertScrollDelta"          "-111"
+        Option      "HorizScrollDelta"         "-111"
+        Option      "TapButton1"       	          "1"
+EndSection
+to your /etc/X11/xorg.conf.d/50-synaptics.conf'
+
