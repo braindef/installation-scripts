2816c7ad6b2669cb579cce6aa607aca4ec76928d
Marc jun Landolt <mail@marclandolt.ch>
2018-06-13 16:04:12 +0200

    changes

--- desktop/gentoo/13_apps.sh
+++ desktop/gentoo/13_apps.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
+emerge --update gdm gnome gnome-terminal nautilus --autounmask-write
+etc-update
 emerge --update gdm gnome gnome-terminal nautilus
+
+#read -n1 -r -p "Press any key to continue or ctrl-c and make menuconfig" key
+
 emerge --update firefox
 emerge --update chromium
 emerge --update inkscape gimp thunderbird vlc libreoffice keepass
 emerge --update app-portage/gentoolkit-next
+
+vim /etc/locale.conf
+systemctl enable dbus
+
