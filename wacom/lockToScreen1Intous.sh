0360a540af559623ef918bc03ece286580ff7fc2
Marc jun Landolt <mail@marclandolt.ch>
2018-08-07 17:12:00 +0200

    intous pen

--- wacom/lockToScreen1Intous.sh
+++ wacom/lockToScreen1Intous.sh
@@ -3,3 +3,4 @@
 xrandr | grep connected | cut -d" " -f1
 sudo apt-get install xinput x11-xserver-utils
 xinput map-to-output $(xinput |grep Pen |grep stylus | cut -d= -f2 | cut -c1-2) DP-2
+xinput map-to-output $(xinput |grep Pen |grep stylus | cut -d= -f2 | cut -c1-2) XWAYLAND1
