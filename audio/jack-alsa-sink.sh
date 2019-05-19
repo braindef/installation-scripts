2b1e1fccd6a30ae4d726cf8a5c98d5e14b5b2130
landev <landev@mail>
2019-05-19 15:37:15 +0200

    tempsave

--- audio/jack-alsa-sink.sh
+++ audio/jack-alsa-sink.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-jackd
+
+qjackctl &
+echo start jack2 server please, then press enter
+read
 pactl load-module module-jack-sink channels=2
 pactl load-module module-jack-source
 pacmd set-default-sink jack_out
+./Jamulus --server &
+./Jamulus
