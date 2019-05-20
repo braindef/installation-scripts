a447936c4e6dfe01ee6e89a17f821d8a5e4428a7
Your Name <you@example.com>
2019-05-20 19:30:46 +0200

    ALSA

--- audio/jack-alsa-sink.sh
+++ audio/jack-alsa-sink.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-qjackctl &
 echo start jack2 server please, then press enter
 read
 pactl load-module module-jack-sink channels=2
 pactl load-module module-jack-source
-pacmd set-default-sink jack_out
-./Jamulus --server &
-./Jamulus
+Jamulus --server &
+Jamulus 
