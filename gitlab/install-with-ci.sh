a7f44e72f91cba1c767ec662bff4f77ec3c76017
Your Name <you@example.com>
2019-07-22 02:20:35 +0200

    tempsave

--- gitlab/install-with-ci.sh
+++ gitlab/install-with-ci.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
+
+
+#SOURCE: https://www.digitalocean.com/community/tutorials/how-to-install-and-use-docker-on-ubuntu-16-04
+sudo apt update
+sudo apt install apt-transport-https ca-certificates curl gnupg2 software-properties-common
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
+sudo apt update
+apt-cache policy docker-ce
+sudo apt install docker-ce
+sudo systemctl status docker
+
+
+#SORUCE: https://www.digitalocean.com/community/tutorials/how-to-set-up-continuous-integration-pipelines-with-gitlab-ci-on-ubuntu-16-04
+
 curl https://packages.gitlab.com/install/repositories/gitlab/gitlab-ee/script.deb.sh | sudo bash
 curl -L https://packages.gitlab.com/install/repositories/runner/gitlab-runner/script.deb.sh | sudo bash
 sudo apt-get install gitlab-ee gitlab-runner
@@ -6,3 +21,6 @@ sudo gitlab-ctl reconfigure
 sudo gitlab-runner register
 sudo gitlab-runner start
 sudo gitlab-ctl reconfigure
+
+
+
