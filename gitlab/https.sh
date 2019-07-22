c5026290a476b4eda4a7ca2ca24660200053791e
marc <marc@server.home>
2019-07-23 00:25:57 +0200

    gitlab

--- gitlab/https.sh
+++ gitlab/https.sh
@@ -2,7 +2,9 @@
 #!/bin/bash
 sudo openssl genrsa -des3 -out /etc/gitlab/ssl/git.0x8.ch.key 2048
 
-sudo openssl req -new -key /etc/gitlab/ssl/git.0x8.ch.key -out /etc/gitlab/ssl/git.0x8.ch.csr
+#sudo openssl req -new -key /etc/gitlab/ssl/git.0x8.ch.key -out /etc/gitlab/ssl/git.0x8.ch.csr
+
+sudo openssl req -nodes -newkey rsa:2048 -keyout /etc/gitlab/ssl/git.0x8.ch.key -out /etc/gitlab/ssl/git.0x8.ch.csr
 
 sudo cp -v /etc/gitlab/ssl/git.0x8.ch.{key,original}
 
