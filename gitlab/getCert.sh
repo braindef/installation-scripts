7c723b03a3c7775087fab0c106524cedd5a46c14
Your Name <you@example.com>
2019-08-15 17:37:36 +0200

    tempsave

---
+++ gitlab/getCert.sh
@@ -0,0 +1,7 @@
+#bin/bash
+
+echo copy the cert
+openssl s_client -connect git.0x8.ch:443
+
+
+
---
+++ gitlab/installCAcert.sh
@@ -0,0 +1,5 @@
+mkdir -p /usr/share/ca-certificates/gitlab
+cp ~/git.0x8.ch.cert /usr/local/share/ca-certificates/gitlab/git.0x8.ch.crt
+chmod 755 /usr/local/share/ca-certificates/gitlab
+chmod 644 /usr/local/share/ca-certificates/gitlab/git.0x8.ch.crt
+update-ca-certificates
