8d52aa64648929e414407082e0b768e7f06ab29f
Your Name <you@example.com>
2019-08-15 17:45:48 +0200

    tempsave

---
+++ gitlab/installDocker.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+sudo apt-get install \
+     apt-transport-https \
+     ca-certificates \
+     curl \
+     gnupg2 \
+     software-properties-common
+     
+curl -fsSL https://download.docker.com/linux/$(. /etc/os-release; echo "$ID")/gpg | sudo apt-key add -
+
+sudo apt-key fingerprint 0EBFCD88
+
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/$(. /etc/os-release; echo "$ID") \
+   $(lsb_release -cs) \
+   stable"
+   
+sudo apt-get install docker-ce
