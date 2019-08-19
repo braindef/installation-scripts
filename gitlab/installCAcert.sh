cda83064ba03823f024b62fc83d872c294e593ae
Your Name <you@example.com>
2019-08-19 11:03:25 +0200

    fixing wrong dir

--- gitlab/installCAcert.sh
+++ gitlab/installCAcert.sh
@@ -1,4 +1,4 @@
-mkdir -p /usr/share/ca-certificates/gitlab
+mkdir -p usr/local/share/ca-certificates/gitlab
 cp ~/git.0x8.ch.cert /usr/local/share/ca-certificates/gitlab/git.0x8.ch.crt
 chmod 755 /usr/local/share/ca-certificates/gitlab
 chmod 644 /usr/local/share/ca-certificates/gitlab/git.0x8.ch.crt
