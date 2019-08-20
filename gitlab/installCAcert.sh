8b3720b347b3dabed8df263219183ba88e953404
Your Name <you@example.com>
2019-08-20 15:35:54 +0200

    cert stuff

--- gitlab/installCAcert.sh
+++ gitlab/installCAcert.sh
@@ -1,4 +1,6 @@
 #/bin/bash
+save certificate to ~/git.0x8.ch.cert
+openssl s_client -connect git.0x8.ch:443
 mkdir -p /usr/local/share/ca-certificates/gitlab
 cp ~/git.0x8.ch.cert /usr/local/share/ca-certificates/gitlab/git.0x8.ch.crt
 chmod 755 /usr/local/share/ca-certificates/gitlab
