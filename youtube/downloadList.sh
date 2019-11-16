2376f690db4bf453e4d191886f9cb077986205e2
Your Name <you@example.com>
2019-11-16 18:30:41 +0100

    tempsave

--- youtube/downloadList.sh
+++ youtube/downloadList.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-youtube-dl -o "%(title)s.%(ext)s" $1
+youtube-dl -i -o "%(title)s.%(ext)s" $1
 
 #youtube-dl -o "%(title)s.%(ext)s" https://www.youtube.com/playlist?list=PLWsX0-AjlNWx0OSnVHjA61sKGRcaPpSIa
 
