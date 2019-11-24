47d4d0a190bfdce25d4dc546c90182f0d813c346
Your Name <you@example.com>
2019-11-24 01:51:01 +0100

    survaillance

---
+++ video/ubuntu/eoan/prerequisites.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+sudo apt-get install v4l-utils ffmpeg vlc
+sudo apt-get install gstreamer1.0-libav
+sudo apt-get install libdvdnav4 libdvdread4 gstreamer1.0-plugins-bad gstreamer1.0-plugins-ugly
--- video/ubuntu/eoan/rec-ffmpeg.sh
+++ video/ubuntu/eoan/rec-ffmpeg.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 
 dir=$(date +%y%m%d%H%M%s)
+
+read -n 1 -s -r -p "press any key to create $dir and start record, to stop press again any key"
 mkdir ./$dir
 cd $dir
 var=0
 for i in $(ls /dev/video*)
 do
   echo $var
-  ffmpeg -f v4l2 -input_format yuyv422 -framerate 30 -video_size 640x480 -i $i -c:v libx264 -vf format=yuv420p cam$var.mjpg &
+  #ffmpeg -f v4l2 -input_format yuyv422 -framerate 30 -video_size 1280x720 -i $i -preset ultrafast -c:v libx264 cam$var.mjpg &
+  #ffmpeg -f v4l2 -input_format yuyv422 -framerate 30 -video_size 1280x720 -i $i -preset ultrafast -c:v mpeg4 cam$var.mjpg &
+  #ffmpeg -f v4l2 -input_format yuyv422 -framerate 30 -video_size 1280x720 -i $i -preset ultrafast -c:v libx265 cam$var.mjpg &
+  ffmpeg -f v4l2 -input_format yuyv422 -framerate 30 -video_size 1280x720 -i $i -preset ultrafast -c:v libx264 cam$var.mjpg &
   var=$((var+1))
 done
+
+read -n 1 -s -r -p "Press any KEY to stop"
+killall ffmpeg
